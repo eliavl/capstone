b2b9429b88b1ff847df82fb8de2e9c7c8e701431
unknown <elijahmalikalexander@gmail.com>
2019-08-01 23:29:49 -0400

    capstone project

---
+++ src/main/java/com/techtalentsouth/TechTalentBlog/TechTalentblogApplication.java
@@ -0,0 +1,13 @@
+package com.techtalentsouth.TechTalentBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechTalentblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechTalentblogApplication.class, args);
+	}
+
+}
